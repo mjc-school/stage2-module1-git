76644aac19a97340091bca34ef0125354d775f77
ShafkatS <126591704+ShafkatS@users.noreply.github.com>
2023-06-05 18:00:40 +0500

    Update Repo.java

--- src/main/java/com/epam/git/Repo.java
+++ src/main/java/com/epam/git/Repo.java
@@ -1,5 +1,5 @@
 package com.epam.git;
 
 public class Repo {
-    public static String REPO_LINK = "https://github.com/ShafkatS/stage2-module1-git1";
+    public static String REPO_LINK = "https://github.com/ShafkatS/stage2-module1-git";
 }
